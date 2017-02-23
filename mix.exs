a0293bd469c816a9cfa320ad2e59c983cd2dacea
Eric Scouten <eric@scouten.com>
2017-02-23 11:00:50 -0800

    OK, one more attempt to bypass mix deps.get.

--- mix.exs
+++ mix.exs
@@ -18,14 +18,14 @@ defmodule Sqlite.Ecto.Mixfile do
   end
 
   defp deps do
-    [{:backoff, git: "https://github.com/scouten/backoff.git", ref: "8f10cb83b5fbc2401e6a06b341417cad4c632f34", override: true},
-     {:db_connection, git: "https://github.com/elixir-ecto/db_connection", ref: "625e42521e2b72f597c388daa6ecf5a90d7bd6e7", override: true}, # version 0.1.7
-     {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "785903ae6d157ebda824e4e841f1f63aa93f9208", override: true},
-     {:ecto, git: "https://github.com/scouten/ecto.git", ref: "b738eb1967bac6c0ab1f56925a1ed19a6fb5413f"},
-     {:poison, "~> 1.0"},
-     {:postgrex, git: "https://github.com/ericmj/postgrex.git", ref: "f773f8d4b88565aea435c6651b711ebfce74c9c2", override: true},
-     {:sbroker, "~> 1.0", override: true},
-     {:sqlitex, git: "https://github.com/scouten/sqlitex.git", ref: "8f1dcd4107cd99ca0687bf870b914e44a467722d", override: true}]
+    [{:backoff, path: "deps/backoff", override: true},
+     {:db_connection, path: "deps/db_connection", override: true},
+     {:esqlite, path: "deps/esqlite", override: true},
+     {:ecto, path: "deps/ecto", override: true},
+     {:poison, path: "deps/poison", override: true},
+     {:postgrex, path: "deps/postgrex", override: true},
+     {:sbroker, path: "deps/sbroker", override: true},
+     {:sqlitex, path: "deps/sqlitex", override: true}]
   end
 
   defp package do
