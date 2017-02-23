357ecb460734041c16ed41b18ef73c022e73a39b
Eric Scouten <eric@scouten.com>
2017-02-23 10:25:47 -0800

    Remove coverage infrastructure.

--- mix.exs
+++ mix.exs
@@ -9,40 +9,24 @@ defmodule Sqlite.Ecto.Mixfile do
      elixirc_options: [warnings_as_errors: true],
      deps: deps(),
      elixirc_paths: elixirc_paths(Mix.env),
-
-     # testing
-     build_per_environment: false,
-     test_paths: test_paths(),
-     test_coverage: [tool: Coverex.Task, coveralls: true],
-
-     # hex
-     description: description(),
      package: package(),
-
-     # docs
-     docs: [main: Sqlite.Ecto]]
+     test_paths: test_paths()]
   end
 
-  # Configuration for the OTP application
   def application do
     [applications: [:db_connection, :ecto, :logger]]
   end
 
-  # Dependencies
   defp deps do
     [{:backoff, git: "https://github.com/scouten/backoff.git", ref: "8f10cb83b5fbc2401e6a06b341417cad4c632f34", override: true},
-     {:coverex, "~> 1.4.11", only: :test},
      {:db_connection, git: "https://github.com/elixir-ecto/db_connection", ref: "625e42521e2b72f597c388daa6ecf5a90d7bd6e7", override: true}, # version 0.1.7
      {:esqlite, git: "https://github.com/mmzeeman/esqlite", ref: "785903ae6d157ebda824e4e841f1f63aa93f9208", override: true},
-     {:ex_doc, "~> 0.14.5", only: :dev},
      {:ecto, git: "https://github.com/scouten/ecto.git", ref: "b738eb1967bac6c0ab1f56925a1ed19a6fb5413f"},
      {:poison, "~> 1.0"},
      {:postgrex, git: "https://github.com/ericmj/postgrex.git", ref: "f773f8d4b88565aea435c6651b711ebfce74c9c2", override: true},
      {:sbroker, "~> 1.0", override: true},
      {:sqlitex, git: "https://github.com/scouten/sqlitex.git", ref: "8f1dcd4107cd99ca0687bf870b914e44a467722d", override: true}]
   end
-
-  defp description, do: "SQLite3 adapter for Ecto2 (not yet working)"
 
   defp package do
     [maintainers: ["Jason M Barnes", "Eric Scouten"],
